c07803b8d8314b9e488e977e97d662b32d04c39e
xiaozhazha220417 <2053678428@qq.com>
2022-06-21 09:25:03 +0800

    项目初始完成，素材与目录结构搭建完毕

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,20 @@
 <template>
   <div id="app">
     <h1>黑马头条</h1>
-    <router-link></router-link>
+    <i class="toutiao toutiao-lishi"></i>
+    <br />
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+
+    <van-cell-group>
+      <van-cell title="单元格" value="内容" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+    </van-cell-group>
+
+    <p class="ceshi">测试</p>
   </div>
 </template>
 
@@ -20,4 +33,7 @@ export default {
 </script>
 
 <style scoped lang='less'>
+.ceshi {
+  font-size: 20px;
+}
 </style>
