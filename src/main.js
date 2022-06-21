68a7421b109d2c5893cf1b9f80f7cd7db027f7d5
xiaozhazha220417 <2053678428@qq.com>
2022-06-21 10:28:43 +0800

    初始化项目目录以及文件优化

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 引入全局样式
 import '@/styles/index.less'
+// 引入处理响应式rem界面的文件
 import 'amfe-flexible'
 
+// vant 组件库的注册和使用
 import Vant from 'vant'
 import 'vant/lib/index.css'
 Vue.use(Vant)
